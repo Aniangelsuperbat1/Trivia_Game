fc1ab1ee196018591a232102efa7566ce2f993d9
Patrick <zhen.patrick@gmail.com>
2021-02-19 15:54:12 -0500

    Added next button functionality

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const counter = document.querySelector("#counter")
+const score = document.querySelector("#score")
+const keepTrack = document.querySelector("#keepTrack")
 let question = document.querySelector("#question")
 const btn1 = document.querySelector("#start")
 const btn2 = document.querySelector("#next")
 const answer = document.querySelector("#answer")
 const subAnswer = document.querySelector("#subAnswer")
-
 
 const trivia = [
     {
@@ -23,29 +24,44 @@ const trivia = [
 
 // Swal.fire('Any fool can use a computer')
 
-let rando = trivia[Math.floor(Math.random() * trivia.length)]
+let newScore = 0
 
-trivia.forEach((quest) => {
-    subAnswer.addEventListener("click", () =>{
-        if(answer.value !== quest.answer){
-            alert("wrong answer")
-        } else {
-            alert("right answer")
-        }
-    })
-})
+let rando = () =>{
+    return trivia[Math.floor(Math.random() * trivia.length)]
 
-let score = 0
+}
 
 let manOfSteel = () => {
-    question.innerHTML = rando.question
+    question.innerHTML = rando().question
     btn1.style.display = "none"
 }
 
 let darkKnight = () => {
-    console.log("need")
-    question.innerHTML = rando.question
+    console.log("fsdf")
+    let nextQuestion = rando().question
+    question.innerHTML = nextQuestion
 }
+
+let corp = () => {
+    answer.value = ""
+    console.log("jhg")
+    trivia.forEach((triv) =>{
+        if(question.innerHTML === triv.question && answer.value !== triv.answer){
+        alert("wrong answer") 
+        let currScore = newScore--
+        keepTrack.innerHTML = currScore
+        console.log(currScore)}
+        
+        else if (question.innerHTML === triv.question && answer.value === triv.answer){ 
+            alert("right answer")
+            let currScore1 = newScore++
+            keepTrack.innerHTML = currScore1
+            return currScore
+        }
+    })
+    
+}
+
 
 let startGame = () => {
     btn1.addEventListener("click", manOfSteel)
@@ -55,5 +71,11 @@ let nextQuestion = () => {
     btn2.addEventListener("click", darkKnight)
 }
 
+let brightestDay = () => {
+    subAnswer.addEventListener("click", corp)
+}
+
+
 startGame()
 nextQuestion()
+brightestDay()
