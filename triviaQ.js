5cd5699eb23bf809a03d9b870e7af531e81302fe
Patrick <zhen.patrick@gmail.com>
2021-02-21 10:42:03 -0500

    added more questions

--- triviaQ.js
+++ triviaQ.js
@@ -14,6 +14,30 @@ const trivia = [
     {
         question: "In what country did the Red Son land in?",
         answer: "Soviet Union"
+    },
+    {
+        question: "Name the 5 Robins in chronological order",
+        Answer: "Dick Grayson Jason Todd, Tim Drake, Stephanie Brown, Damian Wayne"
+    },
+    {
+        question: 'Finish this quote "You\'ve got heart kid, where are you from?"',
+        answer: "Queens"
+    },
+    {
+        question: "Who is Virgil Hawkins alter ego?",
+        answer: "Static Shock"
+    },
+    {
+        question: "What Avenger said...'I can do this all day",
+        answer: "Captain America"
+    },
+    {
+        question: "Who is Wally West's alter ego?",
+        answer: "Kid Flash"
+    },
+    {
+        question: "From where does Ras Al Ghul get his immorality?",
+        answer: "Lazarus Pit"
     }
 ]
 
