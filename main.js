5edbf565935f6065ddd0a221ab66360710afd196
Patrick <zhen.patrick@gmail.com>
2021-02-22 16:31:36 -0500

    deleted forEach loop

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-// import {trivia} from "./triviaQ"
-
 const counter = document.querySelector("#counter")
 let timeLeft = document.querySelector("#timeLeft")
 const score = document.querySelector("#score")
@@ -11,11 +9,12 @@ const answer = document.querySelector(".answer")
 const subAnswer = document.querySelector("#subAnswer")
 const num = document.querySelector(".num")
 
-
 let newScore = 0
 let newLife = 3
-let timeLeft1 = 30
-let reload = false
+let timeLeft1 = 10
+let superHero = null
+let startClick = false
+
 let audioPlay = () => {
     let audio = new Audio("Jeopardy-theme-song.mp3")
     audio.play()
@@ -25,7 +24,7 @@ let gem = () => {
     Swal.fire({
     title: 'Welcome',
     text: '"Lasciate ogni speranza, o voi ch\'intrate"',
-    imageUrl: 'party.gif',
+    imageUrl: './gifs/Avengers_assemble2.gif',
     imageWidth: 400,
     imageHeight: 300,
     imageAlt: 'Custom image',
@@ -33,12 +32,16 @@ let gem = () => {
 }
 
 let rando = () =>{
-    let superHero = trivia[Math.floor(Math.random() * trivia.length)]
+    let randomIndex = Math.floor(Math.random() * trivia.length)
+    superHero = trivia[randomIndex]
+    trivia.splice(randomIndex, 1)
     let heroes = superHero.question
     question.innerHTML = heroes
 }
 
+
 let count = () => {
+    timeLeft1 = 10
     setInterval(() => {
         if(timeLeft1 <= 0){
             clearInterval(timeLeft1 = 0)
@@ -48,100 +51,105 @@ let count = () => {
             // text: 'Better luck next Question',
             // imageUrl: "Smash_bro.gif"})
         }
+        
         timeLeft.innerHTML = timeLeft1
         timeLeft1 -=1
-    }, 1000)
-}
+    }, 1000) 
+    }
 
-let manOfSteel = () => {
+const manOfSteel = () => {
     rando()
     // audioPlay()
     btn1.style.display = "none"
     btn2.classList.remove("hide")
+    startClick = true
     count()
 }
 
-let darkKnight = () => {
+const darkKnight = () => {
     rando()
     count()
 }
 
-// let clickStart = () =>{
-//     if(answer.value.length > 0){
-//         alert("Click Start first")
-//         answer.value = ""
-//     } else if(startGame){
-//         return
-//     }}
+const isClicked = () => {
+    if(startClick === false){
+        Swal.fire({
+            icon: 'warning',
+            title: 'Push the START button first',
+            text: 'Don\'t be a hero',
+            imageUrl: "./gifs/push_button.gif"})
+        }
+}
 
-let laternCorp = () => {
-    // clickStart()
-    let removeQuestion = trivia
-    trivia.forEach((triv, index) =>{
-        if(question.innerHTML === triv.question && answer.value !== triv.answer){
+const answered = () => {
+    if(answer.value !== superHero.answer){
         Swal.fire({
             icon: 'error',
             title: 'Wrong Answer!',
             text: 'Not Tremendous',
-            imageUrl: "wrong.gif"})
+            imageUrl: "./gifs/wrong.gif"})
         newLife -= 1
         num.innerHTML = newLife
         answer.value = ""
         rando()
-        removeQuestion.splice(index, 1)
-        console.log(trivia)
-            if(newLife === 0){
-                Swal.fire({
-                icon: 'error',
-                title: 'YOU HAVE LOST THE GAME',
-                text: 'You have no health remaining',
-                imageUrl: "game_over.gif",
-            })
-        }}
-        else if (question.innerHTML === triv.question && answer.value === triv.answer){ 
-            Swal.fire({
+    } else{
+         Swal.fire({
             icon: 'success',
             title: 'Correct!',
             text: 'You are doing Great!',
-            imageUrl: "Kobe.gif"})
-            newScore += 1
-            keepTrack.innerHTML = newScore
-            answer.value = ""
-            rando()
-            removeQuestion.splice(index, 1)
-            console.log(trivia)
-                if(newScore === 2){
-                    Swal.fire({
-                        title: 'CONGRATULATIONS ON YOU WIN',
-                        width: 600,
-                        padding: '3em',
-                        background: '#fff url(main-dccomics.jpg)',
-                        backdrop: `
-                        rgba(0,0,123,0.4)
-                        url('symbols.gif')
-                        left top
-                        no-repeat`
-                    })
-                    newScore
-                }
-        }
-    })
-    // count()
+            imageUrl: "./gifs/iron_man.gif"})
+        newScore += 1
+        keepTrack.innerHTML = newScore
+        answer.value = ""
+        rando()
+    }
+    // if(answer.value.length > 0){
+    //         answer.value = ""
+    //     }
 }
 
+const winOrLose = () => {
+    if(newLife === 0){
+        Swal.fire({
+        icon: 'error',
+        title: 'YOU HAVE LOST THE GAME',
+        text: 'You have no health remaining',
+        imageUrl: "./gifs/game_over.gif",
+    })
+}   else if (newScore === 2) {
+        Swal.fire({
+            title: 'CONGRATULATIONS ON YOUR WIN',
+            width: 600,
+            padding: '3em',
+            background: '#fff url(main-dccomics.jpg)',
+            backdrop: `
+            rgba(0,0,123,0.4)
+            url('./gifs/symbols.gif')
+            left top
+            no-repeat`
+        })
+    }  
+}
 
-let startGame = () => {
+const laternCorp = () => {
+    isClicked()
+    answered()
+    winOrLose()
+
+}
+    
+
+const startGame = () => {
     btn1.addEventListener("click", manOfSteel)
 }
 
-let nextQuestion = () => {
+const nextQuestion = () => {
     btn2.addEventListener("click", darkKnight)
 }
 
-let brightestDay = () => {
+const brightestDay = () => {
     subAnswer.addEventListener("click", laternCorp)
 }
-
 
 gem()
 startGame()
